f2fe9050dc6baebb6ac83fed00cc1d04f8bfc113
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-17 02:03:26 -0300

    Se agregó el componente Mensaje y se instaló react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Mensaje from './components/Mensaje'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <>
       <div>
-
+        <Mensaje></Mensaje>
       </div>
     </>
   )
