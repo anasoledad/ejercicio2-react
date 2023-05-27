7934a4a11471482c5010811c466abab5c34e9821
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-26 23:33:15 -0300

    Se cambio el color de fondo

--- src/components/Mensaje.jsx
+++ src/components/Mensaje.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Mensaje = ({nuevoMensaje}) => {
     return (
-        <section className='bg-primary bg-opacity-25 py-5'>
+        <section className='py-5'>
             <h1 className='text-center text-danger'>Hello {nuevoMensaje}</h1>
         </section>
     );
